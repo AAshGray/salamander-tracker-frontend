c0b965690215b667e74aedbd1465c1a3ed8ac2b7
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-11-13 14:08:19 -0800

    added range and color picker to preview processing page

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,12 +1,14 @@
-
+import Link from "next/link"
 export default function Videos() {
     
     return (
         <main>
             <h1>Salamander Video List</h1>
             <ul>
-                <li>Video - </li>
+                {/* link components goes here */}
+                <li><Link href="videos/preview">Example List Item</Link></li>
+                
             </ul>
         </main>
-    )
+    );
 }
--- app/videos/preview/page.tsx
+++ app/videos/preview/page.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link"
+import Image from "next/image"
+
 export default function Preview() {
     return (
-        <p>Processor page</p>
+        <div>
+            <p>Processor page</p>
+            {/* Image placeholder */}
+            <label htmlFor="color-picker">Target Color: 
+                <input type="color" id="color-picker" />
+            </label>
+            <label htmlFor="range-slider">Threshold: 
+                    <input type="range" id="range-slider" min="0" max="455" defaultValue={50}/>
+            </label>
+            
+            {/* Image placeholder */}
+        </div>
     )
 }
