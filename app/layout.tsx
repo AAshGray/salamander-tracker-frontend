56ff1edb08387887c58ea8db71c2eaf62f89df3c
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-11-13 13:30:28 -0800

    basic layout of web page links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +25,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Front Page</Link></li>
+              <li>
+                <Link href="/videos">Video List</Link></li>
+            </ul>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
