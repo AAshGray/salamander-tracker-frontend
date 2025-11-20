965372638b6574ce7c634400bae5c843cc053c15
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2025-11-20 13:32:39 -0800

    Added flexbox to videos page

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -6,13 +6,16 @@ import sala2 from "../../public/VideoSala2.jpg"
 export default function Videos() {
     
     return (
-        <main>
+        <main id="video-page">
             <Image src={sala1} alt="" width={300} height={1000}></Image>
-            <h1>Salamander Video List</h1>
-            <ul>
-                {/* link components goes here */}
-                <li><Link href="videos/preview">Example List Item</Link></li>
-            </ul>
+            <div id="SalaList">
+                <h1>Salamander Video List</h1>
+                <ul>
+                    {/* link components goes here */}
+                    <li><Link href="videos/preview">Example List Item</Link></li>
+                </ul>
+            </div>
+            
             <Image src={sala2} alt="" width={300} height={1000}></Image>
         </main>
     );
