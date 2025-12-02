5d5ddca1f8e6c9514dce24a8d4930b87c7cd1692
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-12-02 12:59:52 -0800

    added fetch to video list; updated fetch file to add functions for other routes

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -4,18 +4,26 @@ import sala1 from "@/public/VideoSala1.jpg"
 import sala2 from "@/public/VideoSala2.jpg"
 import VideoList from "@/components/videolist"
 import {useState, useEffect} from 'react'
-import { fetchVideos } from "@/lib/fetchVideos";
-import {fakeVideos} from "@/mock/mock"
+import {fetchVideos} from '@/lib/fetch'
 
 export default function Page() {
     const [videos, setVideos] = useState<string[]>([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         async function loadVideos() {
-            const vids = await fetchVideos();
-            console.log('Videos loaded in component:', vids);
-            setVideos(vids);
+            try {
+                setLoading(true);
+                const vids = await fetchVideos();
+                console.log('Videos loaded in component:', vids);
+                setVideos(vids);
+            } catch (err: PromiseRejectionEvent | unknown) {
+                console.error('Error loading videos:', err);
+                setError(err instanceof Error ? err.message : String(err));
+            } finally {
+                setLoading(false);
+            }
         }
 
         loadVideos();
@@ -28,6 +36,8 @@ export default function Page() {
                 <h1>Salamander Video List</h1>
                 <VideoList 
                     videos={videos}
+                    loading={loading}
+                    error={error}
                 />
             </div>
             
--- components/videolist.tsx
+++ components/videolist.tsx
@@ -2,12 +2,18 @@ import Link from 'next/link'
 
 type Props = {
   videos: string[];
+  loading: boolean | null;
+  error: string | null;
 };
 
-export default function VideoList({ videos }: Props) {
+export default function VideoList({ videos, loading, error }: Props) {
   
-  if (!videos || videos.length === 0) {
+  if (loading == true) {
     return <ul><li>Loading ...</li></ul>
+  } else {
+    if (loading == false && (!videos || videos.length === 0)) {
+      return <ul><li>There was an error loading videos or the videos directory was empty.</li></ul>
+    }
   }
 
   return (
